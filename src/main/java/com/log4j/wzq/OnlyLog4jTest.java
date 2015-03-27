79d8c8d6705210323be430978ad69646c5c78a84
wangzhenqing <wangzhenqing1008@163.com>
2015-03-27 12:12:26 +0800

    add log4j xml

--- src/main/java/com/log4j/wzq/OnlyLog4jTest.java
+++ src/main/java/com/log4j/wzq/OnlyLog4jTest.java
@@ -1,7 +1,7 @@
 package com.log4j.wzq;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 
 /**
  * @Author:wangzhenqing
@@ -11,13 +11,16 @@ import org.apache.log4j.PropertyConfigurator;
 public class OnlyLog4jTest {
 
     public static void main(String[] args) throws Exception{
-            OnlyLog4jTest onlyLog4jTest = new OnlyLog4jTest();
-            onlyLog4jTest.printInfo();
+        OnlyLog4jTest onlyLog4jTest = new OnlyLog4jTest();
+        onlyLog4jTest.printInfo();
     }
 
     public void printInfo() throws Exception {
 //        BasicConfigurator.configure();
-        PropertyConfigurator.configure(PropertyConfigurator.class.getResourceAsStream("/log4j.properties"));
+//      加载xml文件
+        DOMConfigurator.configure(DOMConfigurator.class.getClassLoader().getResource("log4j.xml"));
+//        加载properties文件
+//        PropertyConfigurator.configure(PropertyConfigurator.class.getResourceAsStream("/log4j.properties"));
 
         Logger logger = Logger.getLogger(OnlyLog4jTest.class);
         logger.debug("debugInfo");
@@ -25,8 +28,7 @@ public class OnlyLog4jTest {
         logger.error("errorInfo");
         logger.fatal("fatalInfo");
         logger.warn("warnInfo");
+        System.out.println(logger.getClass());
 
     }
-
-
 }
